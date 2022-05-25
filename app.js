e117c9b9a03cbaa786746f7c49487224c81300c0
Zettir <gerdes@outlook.dk>
2022-05-25 17:34:27 +0200

    New fetch function

---
+++ app.js
@@ -0,0 +1,295 @@
+////////////////////////////////////////
+/////// Fetch from FastAPI
+////////////////////////////////////////
+async function fetchAPI( url, options ) {
+
+    ///// Check for browser support of fetch in the window interface.
+    if ( ! ( 'fetch' in window ) ) {
+        console.log( 'Error: Fetch API not found, please upgrade your browser.' )
+        return
+    }
+    
+    ///// We can safely use fetch from now on.
+
+    ///// Fetch from API.
+    let request = await fetch( url, options )
+        .then( response => response.text() )
+        .then( JSON.parse )
+        .catch( error => console.log( 'Error:', error ) )
+
+    ///// Return the Data.
+    return request
+}
+
+
+
+////////////////////////////////////////
+/////// Get Images
+////////////////////////////////////////
+async function getImages() {
+
+    ///// The URL to the API.
+    var url = 'https://api.printerboks.dk/api/v1/upload/images'
+
+    ///// Request Options for fetch.
+    let options = {
+        ///// *GET, POST, PUT, DELETE, etc.
+        method: 'GET'
+        //method: 'POST' // TEST
+    }
+
+    ///// Request the data from the API.
+    let request = fetchAPI( url, options )
+    
+    ///// Wait for Response of the Request.
+    let response = await request
+
+    ///// If the Response is 'detail' or empty ).
+    ///// Can be used in if statement instead ( Object.keys(response) == 'detail' ).
+    if ( response.detail ) {
+
+        ///// Get the Detail array.
+        let detail = response.detail[0]
+        
+        ///// If loc array in the Detail array contains 'image'.
+        if ( detail.loc.includes( 'image' ) ) {
+            console.log( 'Message:', detail.msg )
+        } else {
+            console.log( 'Error:', response )
+        }
+        
+        ///// End the function.
+        return
+    } else if ( response == '' || response == 'undefined' ) {
+        console.log( 'Error:', response )
+
+        ///// End the function.
+        return
+    }
+
+    ///// Clear all elements in element ( #get-images .inner ). 
+	document.querySelector( '#get-images .inner' ).innerHTML = '';
+
+    console.log( response )
+
+    for( var i = 0; i < response.length; i++ ){
+
+        let element = `
+        <p>${response[i].filename}</p>
+        `
+
+        document.querySelector( '#get-images .inner' ).insertAdjacentHTML( 'afterbegin', element )
+    }
+
+}
+
+
+
+////////////////////////////////////////
+/////// Upload Image
+////////////////////////////////////////
+async function uploadImage() {
+
+    ///// The URL to the API.
+    var url = 'https://api.printerboks.dk/api/v1/upload/images'
+
+    //var formdata = new FormData( querySelector( '#image-form' ) );
+    var formdata = new FormData( document.forms['image-form'] );
+
+    if ( document.forms['image-form']['image'].value == '' ) {
+        console.log( 'Error: The input field is empty!' )
+        
+        ///// End the function.
+        return
+    }
+
+    ///// Request Options for fetch.
+    let options = {
+        ///// *GET, POST, PUT, DELETE, etc.
+        method: 'POST',
+        body: formdata/* ,
+        headers: {
+            'Content-Type': 'application/json'
+        } */
+    }
+
+    ///// Request the data from the API.
+    let request = fetchAPI( url, options )
+    
+    ///// Wait for Response of the Request.
+    let response = await request
+
+    ///// If the Response is 'detail' or empty ).
+    ///// Can be used in if statement instead ( Object.keys(response) == 'detail' ).
+    if ( response.detail ) {
+
+        ///// Get the Detail array.
+        let detail = response.detail[0]
+        
+        ///// If loc array in the Detail array contains 'image'.
+        if ( detail.loc.includes( 'image' ) ) {
+            console.log( 'Message:', detail.msg )
+        } else {
+            console.log( 'Error:', response )
+        }
+        
+        ///// End the function.
+        return
+    } else if ( response == '' || response == 'undefined' ) {
+        console.log( 'Error:', response )
+        
+        ///// End the function.
+        return
+    }
+   
+    ///// Clear elements
+	document.querySelector( '#upload-image .inner' ).innerHTML = '';
+    
+    console.log( response.filename )
+
+    let element = `
+    <p>${response.filename}</p>
+    `
+
+    document.querySelector( '#upload-image .inner' ).insertAdjacentHTML( 'afterbegin', element )
+
+}
+
+
+
+////////////////////////////////////////
+/////// Get Bookings
+////////////////////////////////////////
+async function getBookings() {
+
+    ///// The URL to the API.
+    var url = 'https://api.printerboks.dk/api/v1/bookings/'
+
+    ///// Request Options for fetch.
+    let options = {
+        ///// *GET, POST, PUT, DELETE, etc.
+        method: 'GET'
+        //method: 'POST' // TEST
+    }
+
+    ///// Request the data from the API.
+    let request = fetchAPI( url, options )
+    
+    ///// Wait for Response of the Request.
+    let response = await request
+
+    ///// If the Response is 'detail' or empty ).
+    ///// Can be used in if statement instead ( Object.keys(response) == 'detail' ).
+    if ( response.detail ) {
+
+        ///// Get the Detail array.
+        let detail = response.detail[0]
+        
+        ///// If loc array in the Detail array contains 'image'.
+        if ( detail.loc.includes( 'image' ) ) {
+            console.log( 'Message:', detail.msg )
+        } else {
+            console.log( 'Error:', response )
+        }
+        
+        ///// End the function.
+        return
+    } else if ( response == '' || response == 'undefined' ) {
+        console.log( 'Error:', response )
+
+        ///// End the function.
+        return
+    }
+
+    ///// Clear all elements in element ( #get-images .inner ). 
+	document.querySelector( '#get-bookings .inner' ).innerHTML = '';
+
+    console.log( response )
+
+    for( var i = 0; i < response.length; i++ ){
+
+        let element = `
+        <p><b>Start Date:</b> ${response[i].start_date}</p>
+        <p><b>End Date:</b> ${response[i].end_date}</p>
+        <p><b>Printer:</b> ${response[i].printer_code}</p>
+        <p><b>Product:</b> ${response[i].name_tag_type}</p>
+        <p><b>Code:</b> ${response[i].code}</p>
+        <hr>
+        `
+
+        document.querySelector( '#get-bookings .inner' ).insertAdjacentHTML( 'afterbegin', element )
+    }
+
+}
+
+
+
+////////////////////////////////////////
+/////// Create Booking
+////////////////////////////////////////
+async function createBooking() {
+	// Clear elements
+	document.querySelector("#create-booking .inner").innerHTML = "";
+
+    var formData = new FormData(document.querySelector('#booking-form'));
+
+    var startDate = formData.get('start_date')
+    var endDate = formData.get('end_date')
+    var printerCode = formData.get('printer_code')
+    var nameTagType = formData.get('name_tag_type')
+
+    console.log('start_date:', startDate, 'end_date:', endDate, 'printer_code:', printerCode, 'name_tag_type:', nameTagType)
+
+    //return
+
+    var requestOptions = {
+        method: 'POST',
+    }
+
+    let getImagesRequest = await fetch(`https://api.printerboks.dk/api/v1/bookings/?start_date=${startDate}&end_date=${endDate}&printer_code=${printerCode}&name_tag_type=${nameTagType}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('Error:', error))
+}
+
+
+
+
+
+function get(url) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
+
+        req.onload = function() {
+            // This is called even on 404 etc
+            // so check the status
+            if (req.status == 200) {
+                // Resolve the promise with the response text
+                resolve(req.response);
+            }
+            else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject(Error(req.statusText));
+            }
+        };
+
+        // Handle network errors
+        req.onerror = function() {
+            reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send();
+    });
+}
+function letTry() {
+    get('https://api.printerboks.dk/api/v1/upload/images1').then(function(response) {
+        console.log("Success!", response);
+    }, function(error) {
+        console.error("Failed!", error);
+    })
+}
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="../_favicon/favicon-32x32.png">
+    <title>FastAPI connection</title>
+</head>
+<body>
+    <h1>FastAPI connection</h1>
+
+
+    <section id="get-images" class="container blue">
+        <header>
+            <h2>Get Images</h2>
+            <button class="list-button" onclick="getImages(); return false">Get Images</button>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #get-images -->
+
+
+    <section id="upload-image" class="container blue">
+        <header>
+            <h2>Upload Image</h2>
+            <form id="image-form" name="image-form" action="POST" onsubmit="return false">
+                <input class="image-form-input" type="file" name="image" required>
+                <button class="list-button" onclick="uploadImage(); return false">Upload Image</button>
+            </form>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #upload-image -->
+
+
+    <section id="get-bookings" class="container green">
+        <header>
+            <h2>Get Bookings</h2>
+            <button class="list-button" onclick="getBookings(); return false">Get Bookings</button>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #get-bookings -->
+
+
+    <section id="create-booking" class="container green">
+        <header>
+            <h2>Create Booking</h2>
+            <form id="booking-form" action="POST" onsubmit="return false">
+                <div id="start-end">
+                    <div id="start-date">
+                        <label for="start-date-input">Start Date</label>
+                        <input id="start-date-input" type="date" name="start_date">
+                    </div>
+                    <div id="end-date">
+                        <label for="end-date-input">End Date</label>
+                        <input id="end-date-input" type="date" name="end_date">
+                    </div>
+                </div>
+                <div id="printer-code">
+                    <label for="printer-code-select">Choose a Printer:</label>
+                    <select id="printer-code-select" name="printer_code">
+                        <option value="XDESP95271_p">Printer 1</option>
+                        <option value="W8IL27UCYQ_m">Printer 2</option>
+                        <option value="1OPYKBGXVN_1">Printer 3</option>
+                    </select>
+                </div>
+                <div id="name-tag-type">
+                    <label for="name-tag-type-select">Choose a Product:</label>
+                    <select id="name-tag-type-select" name="name_tag_type">
+                        <option value="4786103">Product 1</option>
+                        <option value="4760100">Product 2</option>
+                        <option value="47150106">Product 3</option>
+                    </select>
+                </div>
+                <button class="list-button" onclick="createBooking(); return false">Create Booking</button>
+            </form>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #create-booking -->
+
+
+    <script src="app.js"></script>
+</body>
+</html>
