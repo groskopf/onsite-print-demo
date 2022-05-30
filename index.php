a0cb6096c36a674a8b77e077d737141021c2f137
Zettir <gerdes@outlook.dk>
2022-05-30 16:59:02 +0200

    New bookings functions

--- index.php
+++ index.php
@@ -68,28 +68,24 @@
     <section id="create-booking" class="container green">
         <header>
             <h2>Create Booking</h2>
-            <form id="booking-form" name="booking-form" action="POST" onsubmit="return false">
-                <div id="start-end">
-                    <div id="start-date">
+            <form name="booking-form" action="POST" onsubmit="return false">
+                <div class="start-end">
+                    <div class="start-date">
                         <label for="start-date-input">Start Date</label>
-                        <input id="start-date-input" type="date" name="start_date">
+                        <input id="start-date-input" name="start_date" type="date">
                     </div>
-                    <div id="end-date">
+                    <div class="end-date">
                         <label for="end-date-input">End Date</label>
-                        <input id="end-date-input" type="date" name="end_date">
+                        <input id="end-date-input" name="end_date" type="date">
                     </div>
                 </div>
-                <div id="printer-code">
-                    <label for="printer-code-select">Choose a Printer:</label>
-                    <select id="printer-code-select" name="printer_code">
+                <div class="printer-tag">
+                    <select class="printer-code-select" name="printer_code">
                         <option value="XDESP95271_p">Printer 1</option>
                         <option value="W8IL27UCYQ_m">Printer 2</option>
                         <option value="1OPYKBGXVN_1">Printer 3</option>
                     </select>
-                </div>
-                <div id="name-tag-type">
-                    <label for="name-tag-type-select">Choose a Product:</label>
-                    <select id="name-tag-type-select" name="name_tag_type">
+                    <select class="name-tag-type-select" name="name_tag_type">
                         <option value="4786103">Product 1</option>
                         <option value="4760100">Product 2</option>
                         <option value="47150106">Product 3</option>
@@ -100,6 +96,73 @@
         </header>
         <div class="inner"></div>
     </section><!-- #create-booking -->
+
+
+    <section id="get-booking-with-code" class="container green">
+        <header>
+            <h2>Get Booking with Code</h2>
+            <form name="get-booking-with-code" action="POST" onsubmit="return false">
+                <div class="booking-code">
+                    <label for="booking-code-input">Booking Code</label>
+                    <input id="booking-code-input" name="booking-code" type="text" required>
+                </div>
+                <button class="list-button" onclick="getBookingWithCode(); return false">Get Booking</button>
+            </form>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #get-booking-with-code -->
+
+    
+    <section id="update-booking-with-code" class="container green">
+        <header>
+            <h2>Update Booking with Code</h2>
+            <form name="update-booking-with-code" action="POST" onsubmit="return false">
+                <div class="booking-code">
+                    <label for="booking-code-input">Booking Code</label>
+                    <input id="booking-code-input" name="booking-code" type="text" required>
+                </div>
+                <div class="start-end">
+                    <div class="start-date">
+                        <label for="start-date-input">Start Date</label>
+                        <input id="start-date-input" name="start-date" type="date" required>
+                    </div>
+                    <div class="end-date">
+                        <label for="end-date-input">End Date</label>
+                        <input id="end-date-input" name="end-date" type="date" required>
+                    </div>
+                </div>
+                <div class="printer-tag">
+                    <select class="printer-code-select" name="printer-code" required>
+                        <option value="XDESP95271_p">Printer 1</option>
+                        <option value="W8IL27UCYQ_m">Printer 2</option>
+                        <option value="1OPYKBGXVN_1">Printer 3</option>
+                    </select>
+                    <select class="name-tag-type-select" name="name-tag-type" required>
+                        <option value="4786103">Product 1</option>
+                        <option value="4760100">Product 2</option>
+                        <option value="47150106">Product 3</option>
+                    </select>
+                </div>
+                <button class="list-button" onclick="updateBookingWithCode(); return false">Update Booking</button>
+            </form>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #update-booking-with-code -->
+
+
+    <section id="delete-booking-with-code" class="container green">
+        <header>
+            <h2>Delete Booking with Code</h2>
+            <form name="delete-booking-with-code" action="POST" onsubmit="return false">
+                <div class="booking-code">
+                    <label for="booking-code-input">Booking Code</label>
+                    <input id="booking-code-input" name="booking-code" type="text" required>
+                </div>
+                <button class="list-button" onclick="deleteBookingWithCode(); return false">Delete Booking</button>
+            </form>
+        </header>
+        <div class="inner"></div>
+    </section><!-- #delete-booking-with-code -->
 
 
     <script src="js/fetch.js"></script>
--- js/bookings.js
+++ js/bookings.js
@@ -53,7 +53,7 @@ async function getBookings() {
     }
 
     ///// Clear all elements in element ( #get-images .inner ). 
-	document.querySelector( '#get-bookings .inner' ).innerHTML = '';
+	document.querySelector( '#get-bookings .inner' ).innerHTML = ''
 
     for( var i = 0; i < response.length; i++ ){
 
@@ -77,8 +77,6 @@ async function getBookings() {
 /////// Create Booking
 ////////////////////////////////////////
 async function createBooking() {
-	// Clear elements
-	document.querySelector( '#create-booking .inner' ).innerHTML = '';
 
     var formData = new FormData( document.forms['booking-form'] );
 
@@ -139,7 +137,7 @@ async function createBooking() {
     }
 
     ///// Clear all elements in element ( #get-images .inner ). 
-	document.querySelector( '#create-booking .inner' ).innerHTML = '';
+	document.querySelector( '#create-booking .inner' ).innerHTML = ''
 
     let element = `
     <p><b>Start Date:</b> ${response.start_date}</p>
@@ -150,4 +148,244 @@ async function createBooking() {
     `
 
     document.querySelector( '#create-booking .inner' ).insertAdjacentHTML( 'afterbegin', element )
+}
+
+
+
+////////////////////////////////////////
+/////// Get Booking with Code
+////////////////////////////////////////
+async function getBookingWithCode() {
+
+    var bookingCode = document.forms['get-booking-with-code']['booking-code'].value
+    
+    if ( ! bookingCode ) {
+        console.log( 'Error: The input field is empty!' )
+        
+        ///// End the function.
+        return
+    }
+    
+    ///// The URL to the API.
+    var url = 'https://api.printerboks.dk/api/v1/bookings/'+bookingCode
+
+    ///// Request Options for fetch.
+    let options = {
+        ///// *GET, POST, PUT, DELETE, etc.
+        method: 'GET'
+    }
+
+    ///// Request the data from the API.
+    ///// fetchAPI( *url, options, 'blob', 'debug' )
+    let request = fetchAPI( url, options )
+    
+    ///// Wait for Response of the Request.
+    let response = await request
+
+    ///// Show the Response in Console Log.
+    console.log(response);
+
+    ///// If the Response is empty or undefined.
+    if ( response == '' || response == undefined ) {
+        console.log( 'Error: The Response is empty or undefined.' )
+
+        ///// End the function.
+        return
+    }
+
+    ///// If the Response is 'detail' or empty ).
+    ///// Can be used in if statement instead ( Object.keys(response) == 'detail' ).
+    else if ( response.detail ) {
+
+        ///// Get the Detail array.
+        let detail = response.detail
+
+        for( var i = 0; i < detail.length; i++ ){
+
+            ///// If loc array in the Detail array contains 'image'.
+            if ( detail[i].loc[1] ) {
+                console.log( 'Message:', detail[i].loc[1], detail[i].msg )
+            } else {
+                console.log( 'Error:', response )
+            }
+       
+        }
+        
+        ///// End the function.
+        return
+    }
+
+    ///// Clear all elements in element ( #get-images .inner ). 
+	document.querySelector( '#get-booking-with-code .inner' ).innerHTML = ''
+
+    let element = `
+    <p><b>Start Date:</b> ${response.start_date}</p>
+    <p><b>End Date:</b> ${response.end_date}</p>
+    <p><b>Printer:</b> ${response.printer_code}</p>
+    <p><b>Product:</b> ${response.name_tag_type}</p>
+    <p><b>Code:</b> ${response.code}</p>
+    `
+
+    document.querySelector( '#get-booking-with-code .inner' ).insertAdjacentHTML( 'afterbegin', element )
+
+}
+
+
+
+////////////////////////////////////////
+/////// Update Booking with Code
+////////////////////////////////////////
+async function updateBookingWithCode() {
+
+    var formData = new FormData( document.forms['update-booking-with-code'] )
+
+    var bookingCode = formData.get('booking-code')
+    var startDate = formData.get('start-date')
+    var endDate = formData.get('end-date')
+    var printerCode = formData.get('printer-code')
+    var nameTagType = formData.get('name-tag-type')
+
+    console.log('start_date:', startDate, 'end_date:', endDate, 'printer_code:', printerCode, 'name_tag_type:', nameTagType)
+
+    ///// The URL to the API.
+    var url = `https://api.printerboks.dk/api/v1/bookings/${bookingCode}?start_date=${startDate}&end_date=${endDate}&printer_code=${printerCode}&name_tag_type=${nameTagType}`
+
+    ///// Request Options for fetch.
+    let options = {
+        ///// *GET, POST, PUT, DELETE, etc.
+        method: 'PUT'
+    }
+
+    ///// Request the data from the API.
+    ///// fetchAPI( *url, options, 'blob', 'debug' )
+    let request = fetchAPI( url, options )
+    
+    ///// Wait for Response of the Request.
+    let response = await request
+
+    ///// Show the Response in Console Log.
+    console.log(response);
+
+    ///// If the Response is empty or undefined.
+    if ( response == '' || response == undefined ) {
+        console.log( 'Error: The Response is empty or undefined.' )
+
+        ///// End the function.
+        return
+    }
+
+    ///// If the Response is 'detail' or empty ).
+    ///// Can be used in if statement instead ( Object.keys(response) == 'detail' ).
+    else if ( response.detail ) {
+
+        ///// Get the Detail array.
+        let detail = response.detail
+
+        for( var i = 0; i < detail.length; i++ ){
+
+            ///// If loc array in the Detail array contains 'image'.
+            if ( detail[i].loc[1] ) {
+                console.log( 'Message:', detail[i].loc[1], detail[i].msg )
+            } else {
+                console.log( 'Error:', response )
+            }
+       
+        }
+        
+        ///// End the function.
+        return
+    }
+
+    ///// Clear all elements in element ( #get-images .inner ). 
+	document.querySelector( '#update-booking-with-code .inner' ).innerHTML = ''
+
+    let element = `
+    <p><b>Start Date:</b> ${response.start_date}</p>
+    <p><b>End Date:</b> ${response.end_date}</p>
+    <p><b>Printer:</b> ${response.printer_code}</p>
+    <p><b>Product:</b> ${response.name_tag_type}</p>
+    <p><b>Code:</b> ${response.code}</p>
+    `
+
+    document.querySelector( '#update-booking-with-code .inner' ).insertAdjacentHTML( 'afterbegin', element )
+}
+
+
+
+////////////////////////////////////////
+/////// Delete Booking with Code
+////////////////////////////////////////
+async function deleteBookingWithCode() {
+
+    var bookingCode = document.forms['delete-booking-with-code']['booking-code'].value
+    
+    if ( ! bookingCode ) {
+        console.log( 'Error: The input field is empty!' )
+        
+        ///// End the function.
+        return
+    }
+    
+    ///// The URL to the API.
+    var url = 'https://api.printerboks.dk/api/v1/bookings/'+bookingCode
+
+    ///// Request Options for fetch.
+    let options = {
+        ///// *GET, POST, PUT, DELETE, etc.
+        method: 'DELETE'
+    }
+
+    ///// Request the data from the API.
+    ///// fetchAPI( *url, options, 'blob', 'debug' )
+    let request = fetchAPI( url, options )
+    
+    ///// Wait for Response of the Request.
+    let response = await request
+
+    ///// Show the Response in Console Log.
+    console.log(response);
+
+    ///// If the Response is empty or undefined.
+    if ( response == '' || response == undefined ) {
+        console.log( 'Error: The Response is empty or undefined.' )
+
+        ///// End the function.
+        return
+    }
+
+    ///// If the Response is 'detail' or empty ).
+    ///// Can be used in if statement instead ( Object.keys(response) == 'detail' ).
+    else if ( response.detail ) {
+
+        ///// Get the Detail array.
+        let detail = response.detail
+
+        for( var i = 0; i < detail.length; i++ ){
+
+            ///// If loc array in the Detail array contains 'image'.
+            if ( detail[i].loc[1] ) {
+                console.log( 'Message:', detail[i].loc[1], detail[i].msg )
+            } else {
+                console.log( 'Error:', response )
+            }
+       
+        }
+        
+        ///// End the function.
+        return
+    }
+
+    ///// Clear all elements in element ( #get-images .inner ). 
+	document.querySelector( '#delete-booking-with-code .inner' ).innerHTML = ''
+
+    let element = `
+    <p><b>Start Date:</b> ${response.start_date}</p>
+    <p><b>End Date:</b> ${response.end_date}</p>
+    <p><b>Printer:</b> ${response.printer_code}</p>
+    <p><b>Product:</b> ${response.name_tag_type}</p>
+    <p><b>Code:</b> ${response.code}</p>
+    `
+
+    document.querySelector( '#delete-booking-with-code .inner' ).insertAdjacentHTML( 'afterbegin', element )
+
 }
