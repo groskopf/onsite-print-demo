5f8a1f11a4a1732e5e028c047c2b4535cfe13d31
Zettir <gerdes@outlook.dk>
2025-06-06 03:41:15 +0200

    EB | Changed the Script with new Functions and Outputs.

--- onsiteprint-plugin/blocks/event/block-script-parts/participant/print-participant.js
+++ onsiteprint-plugin/blocks/event/block-script-parts/participant/print-participant.js
@@ -1,8 +1,8 @@
 /* ------------------------------------------------------------------------
  #  JS Part Name: Print Participant
  *  Printing the Participant from the Participant List in the Event Block.
- ?  Updated: 2025-05-30 - 18:40 (Y:m:d - H:i)
- ?  Info: Added the opGetBookingFromSession() function.
+ ?  Updated: 2025-06-06 - 03:40 (Y:m:d - H:i)
+ ?  Info: Changed the Script with new Functions and Outputs.
 ---------------------------------------------------------------------------
  #  TABLE OF CONTENTS:
 ---------------------------------------------------------------------------
@@ -16,7 +16,10 @@
 --------------------------------------------------------------------------- */
 import * as opModuleBasic from '../../../../assets/js/inc/basic.js'
 import { opGetBookingFromSession } from '../../../../assets/js/inc/booking/get-booking-from-session.js'
+import { opGetEvent } from '../../../../assets/js/inc/event/event.js'
+import { opGetTemplate } from '../../../../assets/js/inc/template/template.js'
 import { opGetParticipant } from '../parts.js'
+import { opGetApiData } from '../../../../assets/js/inc/api/get-api-data.js'
 
 /* ------------------------------------------------------------------------
  #  2. Function: Print the Participant from the Participant List
@@ -33,19 +36,88 @@ export async function opPrintParticipant( debug, eventId, participantId ) {
         if ( debug !== true ) debug = false
         if ( debug ) console.group( `${ functionName }()` )
 
-        ///// Get the Booking from Session.
-        await opGetBookingFromSession( debug )
-        
+        ///// Get Booking from Session.
+        const bookingItem = await opGetBookingFromSession( debug )
+
+        ///// Validate the Response from the Get Booking from Session.
+        if ( bookingItem.error !== false ) throw opModuleBasic.opReturnResponse( true, 400, { 
+            message: `Something went wrong getting the Booking!`,
+            line: opModuleBasic.errorLine(),
+            function: functionName
+        } )
+ 
+        ///// Get the Event. 
+        const eventItem = opGetEvent( debug, eventId )
+
+        ///// Validate the Response from the Get Event.
+        if ( eventItem.error !== false ) throw opModuleBasic.opReturnResponse( true, 400, { 
+            message: `Something went wrong getting the Event!`,
+            line: opModuleBasic.errorLine(),
+            function: functionName
+        } )
+
+        ///// Get the Template. 
+        const templateItem = opGetTemplate( debug, eventItem.response.details.eventTemplate )
+
+        ///// Validate the Response from the Get Template.
+        if ( templateItem.error !== false ) throw opModuleBasic.opReturnResponse( true, 400, { 
+            message: `Something went wrong getting the Template!`,
+            line: opModuleBasic.errorLine(),
+            function: functionName
+        } )
+
         ///// Get the Participant.
-        await opGetParticipant( debug, eventId, participantId )
+        const participantItem = await opGetParticipant( debug, eventId, participantId )
+
+        ///// Validate the Response from the Get Participant.
+        if ( participantItem.error !== false ) throw opModuleBasic.opReturnResponse( true, 400, { 
+            message: `Something went wrong getting the Participant!`,
+            line: opModuleBasic.errorLine(),
+            function: functionName
+        } )
+
+        ///// Get Template and Participant from the Response.
+        const template = templateItem.response.details
+        const participant = participantItem.response.details
+
+        ///// Create Variables.
+        let bookingCode = bookingItem.response.details.bookingId
+        let layout = template.templateLayout
+        let columnAmount = template.templateLayoutColumns[0]
+        let imageFilename = template.templateFilenameUploaded
+        let string1 = ( 1 <= columnAmount ) ? participant.line1 : ""
+        let string2 = ( 2 <= columnAmount ) ? participant.line2 : ""
+        let string3 = ( 3 <= columnAmount ) ? participant.line3 : ""
+        let string4 = ( 4 <= columnAmount ) ? participant.line4 : ""
+        let string5 = ( 5 <= columnAmount ) ? participant.line5 : ""
+
+        ///// The URL to the API.
+        let url = `https://api.printerboks.dk/api/v1/name_tags/${ bookingCode }?layout=${ layout }`
+
+        ///// The Body Input to Request Options.
+        let bodyInput = JSON.stringify(
+            {
+            "line_1": string1,
+            "line_2": string2,
+            "line_3": string3,
+            "line_4": string4,
+            "line_5": string5,
+            "image_name": imageFilename,
+            "qr_code": "string"
+            }
+        )
+
+        ///// Print the Participant and Get the Response with the Filename.
+        const filenameResponse = await opGetApiData( debug, 'POST', bodyInput, url, 'json' )
 
         ///// Return the Response.
         return opModuleBasic.opReturnResponse( false, 200, { 
             message: `The Participant was Printed!`, 
             line: opModuleBasic.errorLine(),
-            function: functionName
+            function: functionName,
+            details: filenameResponse.response.details
         }, debug )
-
+        
     } catch( errorResponse ) {
 
         ///// Create Error Details.
