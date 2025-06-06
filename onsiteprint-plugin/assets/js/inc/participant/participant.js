30c6b32bd7909f300fe1628c5b1b458e30b9ea3a
Zettir <gerdes@outlook.dk>
2025-06-06 04:29:23 +0200

    JS | Removed Add Participant Function.

--- onsiteprint-plugin/assets/js/inc/participant/participant.js
+++ onsiteprint-plugin/assets/js/inc/participant/participant.js
@@ -1,32 +1,24 @@
 /* ------------------------------------------------------------------------
  #  JS Part Name: Participant
  *  Template functions related to Participant.
- ?  Updated: 2025-06-06 - 03:54 (Y:m:d - H:i)
- ?  Info: Added New Participant Script with Functions.
+ ?  Updated: 2025-06-06 - 04:28 (Y:m:d - H:i)
+ ?  Info: Removed Add Participant Function.
 ---------------------------------------------------------------------------
  #  TABLE OF CONTENTS:
 ---------------------------------------------------------------------------
-
-    1.  Add Participant
 
     2.  Get Participant
     
     3.  Print Participant
 
 ---------------------------------------------------------------------------
- #  1. Add Participant
---------------------------------------------------------------------------- */
-import { opAddParticipant } from './add-participant.js'
-export { opAddParticipant }
-
-/* ------------------------------------------------------------------------
- #  2. Get Participant
+ #  1. Get Participant
 --------------------------------------------------------------------------- */
 import { opGetParticipant } from './get-participant.js'
 export { opGetParticipant }
 
 /* ------------------------------------------------------------------------
- #  3. Print Participant
+ #  2. Print Participant
 --------------------------------------------------------------------------- */
 import { opPrintParticipant } from './print-participant.js'
 export { opPrintParticipant }
