d67e03c0045fb95167be29eb4162a31b76c9d553
Zettir <gerdes@outlook.dk>
2025-12-16 01:43:38 +0100

    JS | Added new `Redirect Listener` Functions.

--- onsiteprint-plugin/assets/js/inc/listeners.js
+++ onsiteprint-plugin/assets/js/inc/listeners.js
@@ -1,8 +1,8 @@
 /* ------------------------------------------------------------------------
  #  JS Part Name: Listeners Script
  *  Functions with Event Listeners to the OnsitePrint Plugin.
- ?  Updated: 2025-08-07 - 04:07 (Y:m:d - H:i)
- ?  Info: Changed 'opDropdownToggleListener' to 'opToggleClassListener'.
+ ?  Updated: 2025-12-16 - 01:40 (Y:m:d - H:i)
+ ?  Info: Added Redirect Listener Functions.
 ---------------------------------------------------------------------------
  #  TABLE OF CONTENTS:
 ---------------------------------------------------------------------------
@@ -10,6 +10,8 @@
 	1. 	Import Functions from Scripts
 
     2. 	Function: Toggle Class Listener
+
+    3. 	Function: Redirect Listener
 
 ---------------------------------------------------------------------------
  #  1. Import Functions from Scripts
@@ -76,6 +78,81 @@ export function opToggleClassListener( debug, button, toggleElement, toggleEleme
             message: `The Dropdown Toggle Class Listener is Active!`,
             line: opModuleBasic.errorLine(),
             function: functionName
+        } )
+
+    } catch( errorResponse ) {
+
+        ///// Create Error Details.
+        let errorDetails = ( errorResponse.error == true ) ? errorResponse : opModuleBasic.opReturnResponse( false, 400, { 
+            message: errorResponse.message,
+            line: opModuleBasic.errorLine(),
+            function: functionName
+        } )
+
+        ///// Log Error Details in the Console.
+        if ( debug ) console.error( 'ERROR:', errorDetails )
+
+    } finally {
+
+        ///// End the Console Log Group.
+        if ( debug ) console.groupEnd()
+
+    }
+
+}
+
+/* ------------------------------------------------------------------------
+ #  3. Function: Redirect Listener
+--------------------------------------------------------------------------- */
+export function opRedirectListener( debug, button, newUrl ) {
+
+    try {
+
+        ///// Get Function Name.
+        var functionName = opRedirectListener.name
+
+        ///// Set the Debug.
+        ////* Set the Parameter If is not defined (true or false).
+        if ( debug !== true ) debug = false
+        if ( debug ) console.group( `${ functionName }()` )
+
+        ///// Set Dropdown Toggle Listener to the Dropdown Button.
+        opModuleBasic.opListener( 'click', button, async ( event ) => {
+
+            ///// Start the Console Log Group.
+            if ( debug ) console.group( `opRedirectListener()` )
+
+            ///// Stop Propagation from the Event Listener.
+            event.stopPropagation()
+
+            ///// Redirect to the New URL.
+            window.location.href = newUrl
+
+            ///// Console Log Success if Debug.
+            if ( debug ) console.log( 'SUCCESS:', { 
+                message: `No errors were found in the Redirect Listener!`,
+                line: opModuleBasic.errorLine(),
+                function: functionName,
+                details: {
+                    button: button,
+                    url: newUrl
+                }
+            } )
+
+            ///// End the Console Log Group.
+            if ( debug ) console.groupEnd()
+
+        } )
+
+        ///// Console Log Success if Debug.
+        if ( debug ) console.log( 'SUCCESS:', { 
+            message: `The Dropdown Redirect Listener is Active!`,
+            line: opModuleBasic.errorLine(),
+            function: functionName,
+            details: { 
+                button: button, 
+                url: newUrl
+            }
         } )
 
     } catch( errorResponse ) {
