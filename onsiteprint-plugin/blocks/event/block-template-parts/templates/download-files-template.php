5a5f0b2363d9912abd0cb3c98ff0090ae7cd6141
Zettir <gerdes@outlook.dk>
2025-12-26 03:36:35 +0100

    EB | Added Classes to the Download Buttons.

--- onsiteprint-plugin/blocks/event/block-template-parts/templates/download-files-template.php
+++ onsiteprint-plugin/blocks/event/block-template-parts/templates/download-files-template.php
@@ -1,8 +1,8 @@
 <?php
 /* ------------------------------------------------------------------------
  *  Modal Part Name: Download Files Template
- ?  Updated: 2025-11-20 - 04:10 (Y:m:d - H:i)
- ?  Info: Changed the Modal Variable.
+ ?  Updated: 2025-12-26 - 03:36 (Y:m:d - H:i)
+ ?  Info: Added Classes to the Download Buttons.
 ---------------------------------------------------------------------------
  #  The Modal Part - Content
 --------------------------------------------------------------------------- */
@@ -14,15 +14,15 @@
 
         <button type="button" class="op-button-close op-cancel_download-files op-button op-button-size-small op-button-style-outline" data-color="primary-90" data-icon="xmark" data-icon-position="right" data-title-visibility="1">
             <span class="op-icon" role="img" aria-label="X Mark Icon"></span>
-            <span class="op-button-title"><?= esc_attr( $modal['close_button'] ) ?></span>
+            <span class="op-button-title"><?= esc_attr($modal['close_button']) ?></span>
         </button>
 
         <h3 class="op-modal-title" data-icon="download">
             <span class="op-icon" role="img" aria-label="Download"></span>
-            <span class="op-text"><?= esc_attr( $modal_df['title'] ) ?></span>
+            <span class="op-text"><?= esc_attr($modal_df['title']) ?></span>
         </h3>
 
-        <p class="op-modal-description"><?= esc_attr( $modal_df['description'] ) ?></p>
+        <p class="op-modal-description"><?= esc_attr($modal_df['description']) ?></p>
 
     </div>
 
@@ -31,23 +31,18 @@
         <div class="op-modal-overflow">
             <div class="op-modal-overflow__inner op-flex-row">
 
-                <button class="op-button op-button-size-medium op-button-style-outline op-flex-fill"  data-color="primary-90" data-icon="file-csv" data-icon-position="left" onclick="opDownloadEventParticipants('csv', <?= esc_attr( $options['event_id'] ) ?>)">
+                <button class="op-button-download-csv op-button op-button-size-medium op-button-style-outline op-flex-fill" data-color="primary-90" data-icon="file-csv" data-icon-position="left" onclick="opDownloadEventParticipants('csv', <?= esc_attr($options['event_id']) ?>)">
                     <span class="op-icon" role="img" aria-label="CSV File Icon"></span>
-                    <span class="op-button-title"><?= esc_attr( $modal_df['download_csv'] ) ?></span>
+                    <span class="op-button-title"><?= esc_attr($modal_df['download_csv']) ?></span>
                 </button>
-                <button class="op-button op-button-size-medium op-button-style-outline op-flex-fill"  data-color="primary-90" data-icon="file-pdf" data-icon-position="left" onclick="opDownloadEventParticipants('pdf', <?= esc_attr( $options['event_id'] ) ?>)">
+                <button class="op-button-download-pdf op-button op-button-size-medium op-button-style-outline op-flex-fill" data-color="primary-90" data-icon="file-pdf" data-icon-position="left" onclick="opDownloadEventParticipants('pdf', <?= esc_attr($options['event_id']) ?>)">
                     <span class="op-icon" role="img" aria-label="PDF File Icon"></span>
-                    <span class="op-button-title"><?= esc_attr( $modal_df['download_pdf'] ) ?></span>
+                    <span class="op-button-title"><?= esc_attr($modal_df['download_pdf']) ?></span>
                 </button>
 
-            </div><!-- .op-modal-overflow__inner -->   
+            </div><!-- .op-modal-overflow__inner -->
         </div><!-- .op-modal-overflow -->
-        
-<!--         <div class="op-modal-buttons op-flex-col">
-            
-    
-        </div><!-- .op-modal-buttons -->
 
-    </div><!-- .op-modal-content__inner -->   
+    </div><!-- .op-modal-content__inner -->
 
 </template>
